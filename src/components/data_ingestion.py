653ecf6a61069bf0cffe497de68547653e51e886
ShadowAdi <131863842+ShadowAdi@users.noreply.github.com>
2024-02-15 13:12:20 +0530

    HyperTuning Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -65,4 +65,4 @@ if __name__=="__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modelTrainer=ModelTrainer()
-    print(modelTrainer.initiateModelTrainer(train_arr,test_arr))
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
